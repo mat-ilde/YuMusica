0e715282e21cdea7f638b7ca7d235f77afd25c0a
mat-ilde <pascualsoto.matilde@gmail.com>
2022-10-02 16:59:10 +0100

    Adding components

---
+++ spotify/src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MediaplayerComponent } from './components/mediaplayer/mediaplayer.component';
+import { HeaderuserComponent } from './components/headeruser/headeruser.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent,
+    MediaplayerComponent,
+    HeaderuserComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
